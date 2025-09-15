00a6c955ea7ab0bcdfe08a4f40d83fe4283897cc
Obie Zuriel <obiezuriel631@gmail.com>
2025-09-15 08:53:27 +0700

    Kemarin lupa push

--- controller/controller_employee.php
+++ controller/controller_employee.php
@@ -1,60 +1,67 @@
 <?php
-    include_once('../model/model_employee.php');
-    include_once('../model/model_office.php');
+include_once('../model/model_employee.php');
+include_once('../model/model_office.php');
 
-    if(!isset($_SESSION)){
-        session_start();
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+if (!isset($_SESSION['employeelist'])) {
+    $_SESSION['employeelist'] = array();
+}
+
+function createEmployee()
+{
+    $employee = new employee();
+    $employee->setNama($_POST['nama']);
+    $employee->setJabatan($_POST['jabatan']);
+    $employee->setUsia($_POST['usia']);
+    array_push($_SESSION['employeelist'], $employee);
+}
+
+function getAllEmployee()
+{
+    return $_SESSION['employeelist'];
+}
+
+function deleteEmployee($employeeIndex)
+{
+    unset($_SESSION['employeelist'][$employeeIndex]);
+}
+
+function assignEmployeeToOffice($employeeIndex, $officeId)
+{
+    $employeeList = &$_SESSION['employeelist'];
+    if (isset($employeeList[$employeeIndex])) {
+        $employeeList[$employeeIndex]->setOfficeId($officeId);
     }
+}
 
-    if(!isset($_SESSION['employeelist'])){
-        $_SESSION['employeelist'] = array();
+function getCurrentOffice($officeId)
+{
+    if (!isset($_SESSION['officelist'])) {
+        $_SESSION['officelist'] = array();
     }
-
-    function createEmployee(){
-        $employee = new employee();
-        $employee->setNama($_POST['nama']);
-        $employee->setJabatan($_POST['jabatan']);
-        $employee->setUsia($_POST['usia']);
-        array_push($_SESSION['employeelist'], $employee);
-    }
-
-    function getAllEmployee(){
-        return $_SESSION['employeelist'];
-    }
-
-    function deleteEmployee($employeeIndex){
-        unset($_SESSION['employeelist'][$employeeIndex]);
-    }
-
-    function assignEmployeeToOffice($employeeIndex, $officeId){
-        $employeeList = &$_SESSION['employeelist']; 
-        if(isset($employeeList[$employeeIndex])){
-            $employeeList[$employeeIndex]->setOfficeId($officeId);
+    foreach ($_SESSION['officelist'] as $office) {
+        if ($office->getId() === $officeId) {
+            return $office->getNama();
         }
     }
+    return "Unemployed";
+}
 
-    function getCurrentOffice($officeId){
-        foreach($_SESSION['officelist'] as $office){
-            if($office->getId() === $officeId){
-                return $office->getNama();
-            }
-        }
-        return "Unemployed";
-    }
+if (isset($_POST['button_registeremployee'])) {
+    createEmployee();
+    header("Location: ../view/view_employee.php");
+}
 
-    if(isset($_POST['button_registeremployee'])){
-        createEmployee();
-        header("Location: ../view/view_employee.php");
-    }
-
-    if(isset($_GET['deleteID'])){
-        deleteEmployee($_GET['deleteID']);
-        header("Location: ../view/view_employee.php");
-    }
-    if(isset($_POST['button_assign_employee'])){
-        $employeeIndex = $_POST['employee_index'];
-        $officeId = $_POST['office_id'];
-        assignEmployeeToOffice($employeeIndex, $officeId);
-        header("Location: ../view/view_employee_office.php");
-    }
-?>
+if (isset($_GET['deleteID'])) {
+    deleteEmployee($_GET['deleteID']);
+    header("Location: ../view/view_employee.php");
+}
+if (isset($_POST['button_assign_employee'])) {
+    $employeeIndex = $_POST['employee_index'];
+    $officeId = $_POST['office_id'];
+    assignEmployeeToOffice($employeeIndex, $officeId);
+    header("Location: ../view/view_employee_office.php");
+}
